114dde9245d9217784e50c574abc4b1e5e220935
Alex Momotyuk <o.a.momotyuk@gmail.com>
2020-01-15 11:41:35 -0600

    added employees list view

---
+++ Bangazon-RedJags/Controllers/EmployeesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bangazon_RedJags.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Bangazon_RedJags.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public EmployeesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: Employees
+        public async Task<ActionResult> Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT FirstName, LastName, DepartmentId FROM Employee";
+
+                    var reader = await cmd.ExecuteReaderAsync();
+
+                    var employees = new List<Employee>();
+
+                    int FirstNameOrdinal = reader.GetOrdinal("FirstName");
+                    int LastNameOrdinal = reader.GetOrdinal("LastName");
+                    int DepartmentIdOrdinal = reader.GetOrdinal("DepartmentId");
+
+
+                    while (reader.Read())
+                    {
+                        employees.Add(new Employee
+                        {
+                            FirstName = reader.GetString(FirstNameOrdinal),
+                            LastName = reader.GetString(LastNameOrdinal),
+                            DepartmentId = reader.GetInt32(DepartmentIdOrdinal)
+                        });
+                    }
+                    reader.Close();
+                    return View(employees);
+                }
+            }
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Bangazon-RedJags/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bangazon_RedJags.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(55, MinimumLength = 1, ErrorMessage = "First Name length should be between 1 and 55 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(55, MinimumLength = 1, ErrorMessage = "Last Name length should be between 1 and 55 characters")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Department Id is required")]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage = "Employee eMail is required")]
+        [StringLength(55, MinimumLength = 5, ErrorMessage = "eMail length should be between 5 and 55 characters")]
+        public string Email { get; set; }
+        
+        [Required]
+        public bool IsSupervisor { get; set; } = false;
+
+        [Required(ErrorMessage = "Computer Id is required")]
+        public int ComputerId { get; set; }
+
+        //public Computer? Computer { get; set; }
+    }
+}
