481780d2cbde1b805959d422a0ab21f686907a24
unknown <fortunato.mugnano88@gmail.com>
2020-01-15 11:37:10 -0600

    department Index completed

---
+++ Bangazon-RedJags/Models/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon_RedJags.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Department Name is required")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Department Name should be between 2 and 15 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Department budget is required")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Department Budget should be 2 and 15 characters")]
+        public int Budget { get; set; }
+
+        [Display(Name = "Number of Employees")]
+        public int EmployeeNumber { get; set; }
+    }
+}
