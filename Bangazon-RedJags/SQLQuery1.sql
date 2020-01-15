aed169bb6439915f6d5312bbe1dbcf6d4180e447
unknown <fortunato.mugnano88@gmail.com>
2020-01-15 13:27:45 -0600

    query left

--- Bangazon-RedJags/SQLQuery1.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT * FROM Department;
-
-SELECT COUNT(e.Id) as EmployeeCount, d.[Name], d.Id, d.Budget
-FROM Department d
-LEFT JOIN Employee e On d.Id = e.DepartmentId
-GROUP BY d.Name, d.Id, d.Budget;
